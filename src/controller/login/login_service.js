f87a44cde540b32231e91152f79a9084dc4c3dcf
Veli Bacik <vbacik.10@gmail.com>
2021-07-26 00:35:56 +0300

    forgot login and all services has done

--- src/controller/login/login_service.js
+++ src/controller/login/login_service.js
@@ -1,39 +1,45 @@
 
 
 const express = require('express')
-const { firebase } = require('./../../../index')
 const LoginService = express()
-// const admin = require('firebase-admin');
-const loginPath = "/login"
+const loginPath = "/login";
+const registerPath = "/register";
+const forgotPath = "/forgot";
 const LoginModel = require('./login_model');
 
-// const firebase = admin.initializeApp(firebaseConfig);
-LoginService.get(loginPath, async (req, res) => {
+const { StatusCodes } = require('http-status-codes');
 
-    firebase.default.auth().signInWithEmailAndPassword("a", "b").then((result) => {
+const firebaseApp = require('firebase');
+const { validate } = require('express-validation')
 
+LoginService.post(registerPath, validate(LoginModel.loginValidation, {}, {}), async (req, res) => {
+    firebaseApp.auth().createUserWithEmailAndPassword(req.body.email, req.body.password).then((result) => {
         return res.send(result.user);
     }).catch((err) => {
-
+        return res.send(err);
     });
-    // const items = snapshot.docs.map(doc => doc.data());
-    // admin
-    //     .auth()
-    //     .getUserByProviderUid("zD0SY6iWObQSkGotIF9yS9dbTS93")
-
-    // return res.send(items);
 })
 
-LoginService.post(loginPath, (req, res) => {
-    console.log(req.body.userName);
-    console.log(req.body.password);
-    const data = new LoginModel(req.body.userName, req.body.password);
 
-    return res.send({
-        "data": data
+
+LoginService.post(loginPath, validate(LoginModel.loginValidation, {}, {}), (req, res) => {
+    firebaseApp.auth().signInWithEmailAndPassword(req.body.email, req.body.password).then((result) => {
+        return res.send(result.user);
+    }).catch((err) => {
+        return res.status(StatusCodes.NOT_FOUND).send(err);
     });
 });
 
+
+LoginService.post(forgotPath, validate(LoginModel.forgotValidation, {}, {}), (req, res) => {
+    const result = firebaseApp.default.auth().sendPasswordResetEmail(req.body.email).then((result) => {
+        return res.send({ "message": "Password forgot mail sended" });
+
+    }).catch((err) => {
+        return res.status(StatusCodes.NOT_FOUND).send(err);
+    });
+
+});
 
 
 module.exports = { LoginService }
